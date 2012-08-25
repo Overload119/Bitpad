68b1d0f5642cd87ec03ad4cf83befd1f372d8a43
overload119 <budgeneration@gmail.com>
2012-08-25 11:27:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitpad::Application.config.secret_token = '1d64f9aa7c8b22ea3912f76057ddb081639b4b98345c53f11993db73fb08526b926a5480cde20d8f3f7b35b95bf18fd66658a3927f88f22392a1513804240d2a'
